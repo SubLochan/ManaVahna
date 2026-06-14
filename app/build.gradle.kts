9d53e2fd5d888964aaa4ec86af0f4ab2f31af66e
Lochan <sublarilochan123@gmail.com>
2026-06-14 11:52:53 +0530

    Play Version 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.Lochan.ManaVahana"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 6
+    versionName = "1.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
